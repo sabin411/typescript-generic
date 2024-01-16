6db7f92bc708d3355d3e3c24cd458df2015353f3
sabin411 <sherasabin411@gmail.com>
2024-01-16 23:49:04 +0545

    safe function.

--- src/03-art-of-type-arguments/14-safe-function.problem.ts
+++ src/03-art-of-type-arguments/14-safe-function.problem.ts
@@ -2,13 +2,13 @@ import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
 const makeSafe =
-  (func: unknown) =>
+  <TParams extends any[], TReturn>(func: (...args: TParams) => TReturn) =>
   (
-    ...args: unknown
+    ...args: TParams
   ):
     | {
         type: "success";
-        result: unknown;
+        result: TReturn;
       }
     | {
         type: "failure";
@@ -29,7 +29,7 @@ const makeSafe =
     }
   };
 
-it("Should return the result with a { type: 'success' } on a successful call", () => {
+it("Should return the result on a successful call", () => {
   const func = makeSafe(() => 1);
 
   const result = func();
@@ -52,7 +52,7 @@ it("Should return the result with a { type: 'success' } on a successful call", (
             error: Error;
           }
       >
-    >,
+    >
   ];
 });
 
@@ -84,7 +84,7 @@ it("Should return the error on a thrown call", () => {
             error: Error;
           }
       >
-    >,
+    >
   ];
 });
 
