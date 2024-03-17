9d821f75abe3fca72fbd4d10ac83b02d5dfd4e41
sabin411 <sherasabin411@gmail.com>
2024-03-17 22:52:06 +0545

    typescript's weird type inferration.

--- src/03-art-of-type-arguments/16-class-names-creator.problem.ts
+++ src/03-art-of-type-arguments/16-class-names-creator.problem.ts
@@ -2,8 +2,8 @@ import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
 const createClassNamesFactory =
-  (classes: unknown) =>
-  (type: unknown, ...otherClasses: unknown[]) => {
+  <TString extends string>(classes: Record<TString, string>) =>
+  (type: TString, ...otherClasses: string[]) => {
     const classList = [classes[type], ...otherClasses];
     return classList.join(" ");
   };
