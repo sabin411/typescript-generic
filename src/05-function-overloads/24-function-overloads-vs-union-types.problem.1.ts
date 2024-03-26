9413b12083e46bae0a1ab32437f50102fc9e6956
sabin411 <sherasabin411@gmail.com>
2024-03-26 22:56:53 +0545

    function overload vs union type: contrasts.

--- src/05-function-overloads/24-function-overloads-vs-union-types.problem.1.ts
+++ src/05-function-overloads/24-function-overloads-vs-union-types.problem.1.ts
@@ -1,7 +1,9 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-function runGenerator(generator: unknown) {
+function runGenerator(generator: { run: () => string }): string;
+function runGenerator(generator: () => string): string;
+function runGenerator(generator: { run: () => string } | (() => string)) {
   if (typeof generator === "function") {
     return generator();
   }
