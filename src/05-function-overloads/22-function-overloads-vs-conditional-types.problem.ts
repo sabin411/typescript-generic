405b5ea4b7fccf90a2ef24642c027456af198f63
sabin411 <sherasabin411@gmail.com>
2024-03-26 22:37:10 +0545

    22-function-overloads-vs-conditional-types.problem

--- src/05-function-overloads/22-function-overloads-vs-conditional-types.problem.ts
+++ src/05-function-overloads/22-function-overloads-vs-conditional-types.problem.ts
@@ -5,9 +5,11 @@ import { Equal, Expect } from "../helpers/type-utils";
  * This time, let's try and solve this one
  * with function overloads too!
  */
-export const youSayGoodbyeISayHello = (greeting: "goodbye" | "hello") => {
+function youSayGoodbyeISayHello(greeting: "goodbye"): "hello";
+function youSayGoodbyeISayHello(greeting: "hello"): "goodbye";
+function youSayGoodbyeISayHello(greeting: "goodbye" | "hello") {
   return greeting === "goodbye" ? "hello" : "goodbye";
-};
+}
 
 it("Should return goodbye when hello is passed in", () => {
   const result = youSayGoodbyeISayHello("hello");
